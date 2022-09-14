bb53f765ae3f4e7166923121ebd0227083d0e2dd
OMAR ALSAFADI <87979640+omar6533@users.noreply.github.com>
2022-09-14 08:43:51 +0300

    adding new line remote

--- src/gitpractice/Gitpractice.java
+++ src/gitpractice/Gitpractice.java
@@ -16,6 +16,9 @@ public class Gitpractice {
      */
     public static void main(String[] args) {
         System.out.println("hello Git ");
+         System.out.println("new line Git ");
+
+        
     }
     
 }
